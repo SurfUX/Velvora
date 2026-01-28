ca607bc84b229214a4e2a8a93d4e0e318c43832e
SurfUX <tecnetsoftwaresolution@gmail.com>
2026-01-28 09:38:22 +0530

    update content

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 
       <a href="index.php" class="logo d-flex align-items-center me-auto me-xl-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.webp" alt=""> -->
-        <h1 class="sitename">Velvora Travels</h1>
+       <img src="assets/img/logo.png" alt=""> 
+        <!-- <h1 class="sitename">Velvora Travels</h1> -->
       </a>
 
       <nav id="navmenu" class="navmenu">
