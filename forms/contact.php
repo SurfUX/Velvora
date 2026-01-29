eafc2c9afc9de074a3fe26b634d02c956ba329fe
SurfUX <tecnetsoftwaresolution@gmail.com>
2026-01-29 21:10:34 +0530

    Folder Changes

--- forms/contact.php
+++ forms/contact.php
@@ -3,9 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../assets/vendor/phpmailer/src/Exception.php';
-require '../assets/vendor/phpmailer/src/PHPMailer.php';
-require '../assets/vendor/phpmailer/src/SMTP.php';
+require __DIR__ . '/../assets/vendor/phpmailer/Exception.php';
+require __DIR__ . '/../assets/vendor/phpmailer/PHPMailer.php';
+require __DIR__ . '/../assets/vendor/phpmailer/SMTP.php';
 
 $mail = new PHPMailer(true);
 
@@ -14,8 +14,8 @@ try {
     $mail->Host = 'smtp.hostinger.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'info@velvoratours.com';
-    $mail->Password = '#Velvora2026'; // mailbox password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SSL
+    $mail->Password = 'YOUR_EMAIL_PASSWORD'; 
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port = 465;
 
     $mail->setFrom('info@velvoratours.com', 'Velvora Tours');
@@ -23,10 +23,10 @@ try {
     $mail->addAddress('info@velvoratours.com');
 
     $mail->Subject = $_POST['subject'];
-    $mail->Body = 
-        "Name: {$_POST['name']}\n" .
-        "Email: {$_POST['email']}\n\n" .
-        "Message:\n{$_POST['message']}";
+    $mail->Body =
+        "Name: " . ($_POST['name'] ?? '') . "\n" .
+        "Email: " . ($_POST['email'] ?? '') . "\n\n" .
+        "Message:\n" . ($_POST['message'] ?? '');
 
     $mail->send();
     echo "OK";
