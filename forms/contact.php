d16046c4bd8cb70f0e10d1444a34bf88517dbd68
SurfUX <tecnetsoftwaresolution@gmail.com>
2026-01-29 21:13:26 +0530

    Changes

--- forms/contact.php
+++ forms/contact.php
@@ -3,6 +3,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+// Load PHPMailer files (your current structure)
 require __DIR__ . '/../assets/vendor/phpmailer/Exception.php';
 require __DIR__ . '/../assets/vendor/phpmailer/PHPMailer.php';
 require __DIR__ . '/../assets/vendor/phpmailer/SMTP.php';
@@ -10,25 +11,44 @@ require __DIR__ . '/../assets/vendor/phpmailer/SMTP.php';
 $mail = new PHPMailer(true);
 
 try {
+
+    // SMTP configuration (Hostinger)
     $mail->isSMTP();
     $mail->Host = 'smtp.hostinger.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'info@velvoratours.com';
-    $mail->Password = 'YOUR_EMAIL_PASSWORD'; 
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Password = '#Velvora2026'; // 👉 put mailbox password here
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SSL
     $mail->Port = 465;
 
+    // Sender (must be your domain email for Hostinger)
     $mail->setFrom('info@velvoratours.com', 'Velvora Tours');
-    $mail->addReplyTo($_POST['email'], $_POST['name']);
+
+    // Safe reply-to (user email)
+    if (!empty($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $mail->addReplyTo($_POST['email'], $_POST['name'] ?? '');
+    }
+
+    // Receiver
     $mail->addAddress('info@velvoratours.com');
 
-    $mail->Subject = $_POST['subject'];
-    $mail->Body =
-        "Name: " . ($_POST['name'] ?? '') . "\n" .
-        "Email: " . ($_POST['email'] ?? '') . "\n\n" .
-        "Message:\n" . ($_POST['message'] ?? '');
+    // Subject
+    $subject = $_POST['subject'] ?? 'New Travel Inquiry';
+    $mail->Subject = $subject;
 
+    // Message body
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $message = $_POST['message'] ?? '';
+
+    $mail->Body =
+        "Name: $name\n" .
+        "Email: $email\n\n" .
+        "Message:\n$message";
+
+    // Send email
     $mail->send();
+
     echo "OK";
 
 } catch (Exception $e) {
